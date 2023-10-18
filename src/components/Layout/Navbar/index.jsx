d0c7ed3b0bf243a80a42948af1f95ada44084854
YernarT <toktaryernar@gmail.com>
2023-10-18 23:16:59 +0600

    update: :hammer: navbar without login state

--- src/components/Layout/Navbar/index.jsx
+++ src/components/Layout/Navbar/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { GoSearch } from "react-icons/go";
+import { Button } from "@/components/common";
+
 import LogoPNG from "@/assets/image/logo-stackoverflow.png";
 import classes from "./style.module.scss";
 
@@ -7,6 +10,16 @@ export default function Navbar() {
   return (
     <nav className={classes.navbar}>
       <img className="logo" src={LogoPNG} alt="StackOverflow" />
+
+      <div className="search">
+        <GoSearch />
+        <input type="text" placeholder="Search..." />
+      </div>
+
+      <div className="auth-btns">
+        <Button variant="secondary">Log in</Button>
+        <Button>Sign up</Button>
+      </div>
     </nav>
   );
 }
