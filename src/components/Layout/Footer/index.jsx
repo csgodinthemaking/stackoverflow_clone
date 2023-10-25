bb7ca1a9c80a1a7ec6e9944910fcf805e1ace759
YernarT <toktaryernar@gmail.com>
2023-10-25 21:55:15 +0600

    feat: :sparkles: Footer 组件

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
 
-import classes from './style.module.scss';
+// Icons
+import { BsStackOverflow } from "react-icons/bs";
+
+// Scoped style
+import classes from "./style.module.scss";
+// Constants
+import { SOCIAL_MEDIA, LINKS } from "@/constants/footer";
 
 export default function Footer() {
   return (
     <footer className={classes.footer}>
-      Footer
+      <div className="logo-wrap">
+        <BsStackOverflow className="logo" />
+      </div>
+
+      {LINKS.map((item) => (
+        <ul key={item.title} className="list">
+          <li className="title">{item.title}</li>
+          {item.links.map((link) => (
+            <li key={link} className="item">
+              {link}
+            </li>
+          ))}
+        </ul>
+      ))}
+
+      <ul className="social-media">
+        {SOCIAL_MEDIA.map((item) => (
+          <li className="item">{item}</li>
+        ))}
+      </ul>
     </footer>
-  )
+  );
 }
