910d594b709042a5bc1d5507bff3c06729d2aa4c
YernarT <toktaryernar@gmail.com>
2023-10-25 21:55:34 +0600

    feat: :sparkles: footer 区常量

---
+++ src/constants/footer.js
@@ -0,0 +1,45 @@
+export const LINKS = [
+  {
+    title: "STACK OVERFLOW",
+    links: ["Questions", "Help"],
+  },
+  {
+    title: "PRODUCTS",
+    links: ["Teams", "Adverting", "Collectives", "Talent"],
+  },
+  {
+    title: "COMPANY",
+    links: [
+      "About",
+      "Press",
+      "Work Here",
+      "Legal",
+      "Privacy Policy",
+      "Terms of Service",
+      "Contact Us",
+      "Cookie Settings",
+      "Cookie Policy",
+    ],
+  },
+  {
+    title: "STACK EXCHANGE NETWORK",
+    links: [
+      "Technology",
+      "Culture & recreation",
+      "Life & arts",
+      "Science",
+      "Professional",
+      "Business",
+      "API",
+      "Data",
+    ],
+  },
+];
+
+export const SOCIAL_MEDIA = [
+  "Blog",
+  "Facebook",
+  "Twitter",
+  "LinkedIn",
+  "Instagram",
+];
