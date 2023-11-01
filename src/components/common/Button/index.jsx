a40ac32da2aeca1638f510c9f34de8409546b975
YernarT <toktaryernar@gmail.com>
2023-11-01 17:24:50 +0600

    update: :hammer: 改进 Button 组件

--- src/components/common/Button/index.jsx
+++ src/components/common/Button/index.jsx
@@ -2,8 +2,23 @@ import React from "react";
 
 import classes from "./style.module.scss";
 
-export default function Button({ children, variant = "primary" }) {
+/**
+ * 通用 Button 组件
+ * @param {variant} 'primary' | 'secondary' | 'outlined'
+ */
+export default function Button({
+  block = false,
+  children,
+  variant = "primary",
+  className = "",
+}) {
   return (
-    <button className={`${classes.customButton} ${classes[variant]}`}>{children}</button>
+    <button
+      className={`${classes.customButton} ${classes[variant]} ${
+        block && classes.blockBtn
+      } ${className}`}
+    >
+      {children}
+    </button>
   );
 }
